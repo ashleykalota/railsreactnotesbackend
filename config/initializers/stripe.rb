91dd4e8baea667bcfe37e84eca113da75f33f100
ashleykalota <quinnzambia@gmail.com>
2024-11-05 05:38:36 +0200

    Ensure Stripe gem is required before usage

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,9 @@
+require 'stripe'
+
+Rails.configuration.stripe = {
+  publishable_key: ENV['STRIPE_PUBLIC_KEY'],
+  secret_key: ENV['STRIPE_SECRET_KEY']
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
