ed3631da95d735e1f36b9127e338dcc4909666df
ashleykalota <quinnzambia@gmail.com>
2024-12-13 12:13:25 +0200

    Resource change

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Ambulance4
     # Set up CORS to allow requests from your frontend
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3001' # Replace with your frontend domain
+        origins '*' # Replace with your frontend domain
         resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
       end
     end
